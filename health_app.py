d319cddeeb50f18de90da97833e9d63d235a5341
ONIFADE MONSURU OLANREWAJU <onifade617@gmail.com>
2024-09-26 12:35:12 +0100

    Update health_app.py

--- health_app.py
+++ health_app.py
@@ -4,7 +4,6 @@ Created on Thu Sep 26 10:34:11 2024
 
 @author: Awarri User
 """
-
 import streamlit as st
 
 # Set the page configuration
@@ -14,22 +13,22 @@ st.set_page_config(page_title="Health Streamlit App", layout="wide")
 if 'page' not in st.session_state:
     st.session_state.page = 'Home'
 
-# Sidebar for navigation
-st.sidebar.title("Navigation")
-if st.sidebar.button("Home"):
-    st.session_state.page = "Home"
-if st.sidebar.button("Sign Up"):
-    st.session_state.page = "Sign Up"
-if st.sidebar.button("Log In"):
-    st.session_state.page = "Log In"
-
 # Display the selected page
 if st.session_state.page == 'Home':
     st.title("Home Page")
     st.write("Welcome to the Multi-Page Streamlit App!")
+    
+    if st.button("Sign Up"):
+        st.session_state.page = "Sign Up"
+        st.experimental_rerun()  # Reload to update page
+    if st.button("Log In"):
+        st.session_state.page = "Log In"
+        st.experimental_rerun()  # Reload to update page
+
 elif st.session_state.page == 'Log In':
     import log_in  # Ensure this file exists
     log_in.display_page()
+
 elif st.session_state.page == 'Sign Up':
     import sign_up  # Ensure this file exists
     sign_up.display_page()
